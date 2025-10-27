0de324d157db25f0677f17c7ab431ae6dda939f1
UMOJA256 <mugisha25steven@gmail.com>
2025-10-27 10:34:29 +0300

    task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single row into the customers table
+INSERT INTO customers (customer_id, first_name, last_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
