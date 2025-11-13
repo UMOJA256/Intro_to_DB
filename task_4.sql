49fa2ebfbd6a3038f4a785c08fa7ee852c9d8e6f
UMOJA256 <mugisha25steven@gmail.com>
2025-11-13 16:07:20 +0300

    Add files via upload

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,9 @@
--- Select the database
+-- Use the database
 USE alx_book_store;
 
--- Retrieve full column details of the Books table without using DESCRIBE or EXPLAIN
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'Books';
+-- Get full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
+
